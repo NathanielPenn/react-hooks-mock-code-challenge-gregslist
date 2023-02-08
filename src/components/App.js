3801e9c270a7a37251222b9ad966d921d74e6cfb
NatePennebaker <npenne1@aol.com>
2023-02-07 22:37:16 -0500

    3/4 deliverables complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then(res => res.json())
+    .then(data => setItems(data))
+  }, [])
+
+  // const [removedItems, setRemovedItems] = useState([]);
+  function handleDeleteItem(id){
+    const handleDelete = items.filter((item) => item.id !== id)
+    setItems(handleDelete);
+    console.log(handleDelete);
+  }
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer items = {items} handleDelete = {handleDeleteItem}/>
     </div>
   );
 }
